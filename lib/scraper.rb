56467c90846599af25bd5e1488fd27ed48ed8b8b
Josh Ellis <joshe523@gmail.com>
2020-03-05 17:11:53 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 html = open("https://flatironschool.com/")
 
 doc = Nokogiri::HTML(html)
-courses = doc.css("#3c239880-48cf-5ce7-84d0-578546d87586 .title-oE5vT4.title-oE5vT4")
+courses = doc.css("#3c239880-48cf-5ce7-84d0-578546d87586 .title-oE5vT4")
 
 courses.each do |course|
   puts course.text.strip
